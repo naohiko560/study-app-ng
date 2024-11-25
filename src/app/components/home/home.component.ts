099392e1a66ee7b4b9a0281cd5214213e4a4db9e
naohiko560 <private.mail.nao@gmail.com>
2024-11-25 09:14:10 +0900

    ionicをインストール（npm update）

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [IonCol, IonRow, IonGrid, ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
